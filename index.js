d8ded9bf1fe21dbdc849627b337222be1cb16a19
Syed Mir Habib <mirakik.habib@gmail.com>
2023-03-16 22:24:32 +0600

    changes

--- index.js
+++ index.js
@@ -60,21 +60,20 @@ const displayQuiz = (data) => {
 
   data.forEach((quiz, i) => {
     quizContainer.innerHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
-  <div class="flex items-center">
-    <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
-      ${i + 1}
-    </div>
-    <p class="text-gray-800 text-sm">${quiz.quetion}</p>
-  </div>
-  <div class="grid grid-cols-2 gap-4 mt-5">
-    ${displayQuizOptions(quiz.options, i)}
-  </div>
-</div>`;
+      <div class="flex items-center">
+        <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
+          ${i + 1}
+        </div>
+        <p class="text-gray-800 text-sm">${quiz.question}</p>
+      </div>
+      <div class="grid grid-cols-2 gap-4 mt-5">
+        ${displayQuizOptions(quiz.options, i)}
+      </div>
+    </div>`;
   });
 };
 
-// EventListener for quiz submit button
-document.getElementById("submit").addEventlistener("click", () => {
+document.getElementById("submit").addEventListener("click", () => {
   if (answers.length < 6) {
     return;
   }
@@ -108,7 +107,8 @@ document.getElementById("submit").addEventlistener("click", () => {
   }
 
   // data setting on local storage and getting data from local storage
-  let storage = JSON.parse(localStorage.getItem("result"));
+  let storage = JSON.parse(localStorage.getItem("results"));
+  // console.log(storage);
   if (storage) {
     localStorage.setItem(
       "results",
